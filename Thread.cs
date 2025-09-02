4108f4c79394010de0b899003d7b77baf8272a24
Matheus Della Rosa <matheusdellarosa11@gmail.com>
2025-09-02 12:41:42 -0300

    Terceiro Commit

--- Thread.cs
+++ Thread.cs
@@ -5,12 +5,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sistemas-Operacionais;
+namespace Sistemas-Operacionais
 {
     internal class Thread
     {
         private int Id { get; set; }
         private int Estado { get; set; } = 0; // 0 - Pronto, 1 - Executando, 2 - Bloqueado
         private bool FoiExecutado { get; set; }
+
+        // Construtor
+        public Thread(int id)
+        {
+            Id = id;
+            Estado = 0; // Inicia
+            FoiExecutado = false; // Não foi executado ainda
+        }
+
+        // Métodos
+        public void Executar()
+        {
+            Estado = 1;
+            FoiExecutado = true;
+            Console.WriteLine($"Thread - {Id}, Executando");
+        }
+
+        public void Bloqueado()
+        {
+            Estado = 2;
+            Console.WriteLine($"Thread - {Id}, Bloqueado");
+        }
+
+        public void Desbloquear()
+        {
+            Estado = 0;
+            Console.WriteLine($"Thread - {Id}, Pronta");
+        }
     }
+
 }
+
